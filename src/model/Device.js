4003ab5e749bc2639dfe45d4d15e391c23a05485
Ilya Lebedev <bacherko@gmail.com>
2019-12-19 19:20:01 +0300

    fix: fix Device model

--- src/model/Device.js
+++ src/model/Device.js
@@ -2,7 +2,7 @@ const core = require('cyberway-core-service');
 const MongoDB = core.services.MongoDB;
 
 module.exports = MongoDB.makeModel(
-    'Option',
+    'Device',
     {
         deviceId: {
             type: String,
